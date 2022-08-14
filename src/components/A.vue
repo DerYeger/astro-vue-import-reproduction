1270a8c4653b33c511ebb28ccf76b6c826578d2f
Jan Müller <janmueller3698@gmail.com>
2022-08-14 18:36:27 +0200

    feat: initial commit

---
+++ src/components/A.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import B from './B.vue'
+</script>
+
+<template>
+  <div>
+    <span>Hello</span>
+    <B />
+  </div>
+</template>
---
+++ src/components/B.vue
@@ -0,0 +1,3 @@
+<template>
+  <span>World</span>
+</template>
